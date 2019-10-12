cbe8b6690b0d11ef1386d0a3d3c6cef357fbf2e8
Luke <luke.k.rowell@gmail.com>
2019-10-12 06:33:11 -0400

    First commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const pool = new Pool({
-    connectionString: 'postgres://haxrmdohtdpejz:5acd0ec4ab5be0cb9e3016bd47a9763576cdce4e73b66298a89fa7e8a3838983@ec2-23-23-92-204.compute-1.amazonaws.com:5432/d1gavj00p2427f',
+    connectionString: process.env.POKEBASE_URL,
     ssl: true,
 });
 
