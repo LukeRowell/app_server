d587084b901c205d2e0ecb0f4608cb8140b56f1b
Luke <luke.k.rowell@gmail.com>
2019-10-19 16:06:40 -0400

    Added path for sending email from portfolio website

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const nodemailer = require('nodemailer');
 const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 5000;
+let allowedOrigins = ['http://www.lukerowell.com'];
 
 app.use(cors({
-    credentials: true
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
+
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
