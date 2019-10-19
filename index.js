8f54b301c89ad0cc89ff528888874861278b0cb2
Luke <luke.k.rowell@gmail.com>
2019-10-19 18:17:24 -0400

    Added path for sending email from portfolio website

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get('/portfolio/sendmail/:parameters', async (request, response) => {
     const db_response = await fetch(recaptcha_api_url, options);   //send the data over to be inserted to the database
     //const db_json = await db_response.json();
     */
-    response(returnData);
+    response.json(returnData);
 
     /*
     app.use(cors());
