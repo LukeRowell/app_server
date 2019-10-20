518762f26d621ef86a6c8ddbede13b64940235a3
Luke <luke.k.rowell@gmail.com>
2019-10-20 12:54:55 -0400

    Added path for sending email from portfolio website

--- index.js
+++ index.js
@@ -42,15 +42,14 @@ app.get('/portfolio/sendmail/:parameters', async (request, response) => {
     const recaptcha_api_url = 'https://www.google.com/recaptcha/api/siteverify';
     const recaptcha_info = {secretKey}
 
-
-    response.json(returnData);
-    /*
     request('http://www.google.com', function (error, response, body) {
         console.error('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
     });
-    */
+    
+    response.json(returnData);
+    
 
     /*
     const rawResponse = await fetch('https://httpbin.org/post', {
